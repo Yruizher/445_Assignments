a06b00f2cb690b9d90e4690be024b6a55c5eaa42
yruizher <yruizher@asu.edu>
2025-04-03 22:55:46 -0700

    Added HotelsErrors.xml, Changed .xsd namespaces, tweaked submission.cs. Changed NearestAirport to NearstAirport

--- submission.cs
+++ submission.cs
@@ -21,7 +21,7 @@ namespace ConsoleApp1
     public class Program
     {
         public static string xmlURL = "https://github.com/Yruizher/445_Assignments/blob/main/Hotels.xml";
-        public static string xmlErrorURL = "Your Error XML URL";
+        public static string xmlErrorURL = "Your Error XML URL"; //Not made yet
         public static string xsdURL = "https://github.com/Yruizher/445_Assignments/blob/main/Hotels.xsd";
 
         public static void Main(string[] args)
@@ -41,34 +41,42 @@ namespace ConsoleApp1
         // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
-
+          
+            //making a new XmlSchemaSet class
             XmlSchemaSet schema = new XmlSchemaSet();
+
+            //adding the scehma to collection
             schema.Add(null, xsdUrl);
 
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.Schemas = schema;
-            settings.ValidationType = ValidationType.Schema;
+            //defining the validation settings
+            XmlReaderSettings hotelSettings = new XmlReaderSettings();
+
+            //association
+            hotelSettings.Schemas = schema;
+            hotelSettingssettings.ValidationType = ValidationType.Schema;
 
             string error = "No Error";
 
-            settings.ValidationEventHandler = (s, e) =>
+            //adding event handler
+            settings.ValidationEventHandler += ValidationEventHandler = (sender, e) =>
             {
-                error = e.Message;
+                error = $"Validation Error: {e.Message}"; 
             }
 
-            using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
-            {
-                while (reader.Read()) { }
-            }
+            //creating XmlReader object
+            XmlReader reader = XmlReader.Create(xmlUrl, hotelSettings);
 
+            //parsing the file
+            while (reader.Read()) { }
+            
+            //return "No Error" if XML is valid.
             return error;
-            //return "No Error" if XML is valid. Otherwise, return the desired exception message.
         }
 
         public static string Xml2Json(string xmlUrl)
         {
-            
 
+            string jsonText = "Not Completed Yet";
             // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
             return jsonText;
 
