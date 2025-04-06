0db2a275d2c8f45ccabab53a4a1a4824cf95e04e
Yruizher <164976171+Yruizher@users.noreply.github.com>
2025-04-05 18:23:49 -0700

    Update submission.cs

--- submission.cs
+++ submission.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Xml.Schema;
 using System.Xml;
 using System.Net;
 using Newtonsoft.Json;
 using System.IO;
-
-
+using System.Runtime.Remoting.Contexts;
+using System.Runtime;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
 
 
 /**
@@ -22,9 +24,9 @@ namespace ConsoleApp1
     public class Program
     {
         //these are the link to the files I have stored in my GitHub. The GitHub is associated to my school account and the repo is public
-        public static string xmlURL = "https://github.com/Yruizher/445_Assignments/blob/main/Hotels.xml";
-        public static string xmlErrorURL = "https://github.com/Yruizher/445_Assignments/blob/main/HotelsErrors.xml";
-        public static string xsdURL = "https://github.com/Yruizher/445_Assignments/blob/main/Hotels.xsd";
+        public static string xmlURL = "https://yruizher.github.io/445_Assignments/Hotels.xml";
+        public static string xmlErrorURL = "https://yruizher.github.io/445_Assignments/HotelsErrors.xml";
+        public static string xsdURL = "https://yruizher.github.io/445_Assignments/Hotels.xsd";
 
         public static void Main(string[] args)
         {
@@ -43,29 +45,27 @@ namespace ConsoleApp1
         // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
-            //string xmlFromUrl = new WebClient(). DownloadString(xmlUrl);
-            //string xsdFromUrl = new WebClient().DownloadString(xsdUrl);
 
             //making a new XmlSchemaSet class
             XmlSchemaSet schema = new XmlSchemaSet();
 
             //adding the scehma to collection
-            schema.Add(null, xsdUrl);
+            schema.Add(xsdUrl, xsdUrl);
 
             //defining the validation settings
             XmlReaderSettings hotelSettings = new XmlReaderSettings();
 
             //association
             hotelSettings.Schemas = schema;
-            hotelSettingssettings.ValidationType = ValidationType.Schema;
+            hotelSettings.ValidationType = ValidationType.Schema;
 
-            string error = "No Error";
+            string error = "No Errors";
 
             //adding event handler
-            settings.ValidationEventHandler += ValidationEventHandler = (sender, e) =>
+            hotelSettings.ValidationEventHandler += (sender, e) =>
             {
-                error = $"Validation Error: {e.Message}"; 
-            }
+                error = $"Validation Error: {e.Message}";
+            };
 
             //creating XmlReader object
             using (XmlReader reader = XmlReader.Create(xmlUrl, hotelSettings))
@@ -73,18 +73,15 @@ namespace ConsoleApp1
                 try
                 {
                     //parsing the file
-                    while (reader.Read()){ }
+                    while (reader.Read()) { }
                 }
                 catch (Exception ex)
                 {
-                    return "Found Errors";
+                    return $"Exception occurred: {ex.Message}";
                 }
             }
-
-            if (is)
-
-            //return "No Error" if XML is valid.
-            return error;
+                //return "No Error" if XML is valid.
+                return error;
         }
 
         public static string Xml2Json(string xmlUrl)
@@ -93,18 +90,18 @@ namespace ConsoleApp1
             using (WebClient client = new WebClient())
             {
                 //Will use WebClient to download the xml from the url and then convert into deserializable json string
-                xml = client.DownloadString(xmlUrl);
+                string xml = client.DownloadString(xmlUrl);
                 XmlDocument xmlDocument = new XmlDocument();
-                xmlDocument = xmlDocument.LoadXml(xml);
+                xmlDocument.LoadXml(xml);
 
                 string jsonString = JsonConvert.SerializeXmlNode(xmlDocument);
                 JObject json = JObject.Parse(jsonString);
-                jsonText = json.ToString();
+                string jsonText = json.ToString();
                 return jsonText;
             }
 
             // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
-            
+
 
         }
     }
